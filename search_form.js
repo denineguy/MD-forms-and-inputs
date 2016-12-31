c4a9ce2c6a247969288bd7bae2f9c2587dcaf8a6
Denine Guy <denineguy@gmail.com>
2016-12-31 01:04:58 -0500

    Added constraint api to search form

--- search_form.js
+++ search_form.js
@@ -1,46 +1,31 @@
 var searchForm = document.getElementById('search');
-searchForm.addEventListener('click', function(event) {
+// searchForm.addEventListener('click', function(event) {
+function checkSearchForm() {
   event.preventDefault();
-  var criterias = document.getElementById('criteria').value;
+  var criterias = document.getElementById('criteria');
   var form = document.getElementById('form');
   var valid;
   /**
-   * Assume that the form is valid if I don't want to do the final check at
-   * the end
-   */
-  //form.className = 'valid';
-
-  /**
-   * Check if users criteria is valid.  Input is not empty, input is at least 2
+   * Check if users criteria is valid.  Input is not empty, input is at least 3
    * characters long but less than 50 characters long. Input is also alphanumeric.  This field will not take symbols.
    */
-  if (validator.isEmpty(criteria)) {
-    form.className = 'invalid';
+  if (!validator.isEmpty(criteria.value) && ((validator.isOfLength(criteria.value, 3)) && validator.isLength(criteria.value, 50))) {
+    criteria.setCustomValidity('');
   } else {
-    valid = true;
-  }
-
-  if ((validator.isOfLength(criteria, 2)) && validator.isLength(criteria, 50)) {
-    valid = true;
-  } else {
-    form.className = 'invalid';
-  }
-
-  //[TODO]I may decided to take this check out because search fields do take alphanumberic characters.
-  if (validator.isAlphanumeric(criteria)) {
-    valid = true;
-  } else {
-    form.className = 'invalid'
+    criteria.setCustomValidity('Please enter a valid search at between 3-50 characters');
   }
 
   /**
    * Checks that validation are valid.  If they are it sets the *
    * form.className to valid.
    */
-  if (!validator.isEmpty(criteria) && validator.isAlphanumeric(criteria) && (validator.isOfLength(criteria, 2)) && (validator.isLength(criteria, 50))) {
+  if (!validator.isEmpty(criteria.value) && validator.isAlphanumeric(criteria.value) && (validator.isOfLength(criteria.value, 2)) && (validator.isLength(criteria.value, 50))) {
     form.className = 'valid';
+  } else {
+    form.className = 'invalid';
   }
-  //[TODO] [ADD CHECK FOR MININMUM AGE]
+}
 
-  //Another way to do this is to set form.className = 'valid' at the top. and it will only flip when form.className = invalid. Then the above check to ensure every validation is valid is not needed.  The only reason I am not doing that is because I do not want to set the default value to "valid"
-});
+window.onload = function() {
+  document.getElementById('criteria').oninput = checkSearchForm;
+}
