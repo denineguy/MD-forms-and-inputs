8430b21bc7e47a6d08c9754e1fcdde5b60fde305
Denine Guy <denineguy@gmail.com>
2017-01-08 18:41:43 -0500

    updated the signup form

--- signup_form.js
+++ signup_form.js
@@ -29,9 +29,9 @@ signupForm.addEventListener('click', function(event) {
   * characters long. Not checking if the name is alphaNumeric as some people * have hyphenated or apostrophe's in names.
   */
   if (!validator.isEmpty(firstName.value) && validator.isOfLength(firstName.value, 2)) {
-    first.innerHTML = ''
-    firstName.className = 'input';
-    first.removeAttribute('class', 'firstname-error');
+    firstName.className = 'input valid';
+    first.innerHTML = '&#10003';
+    first.className = 'valid';
   } else {
     first.innerHTML = 'Please enter at least 2 characters';
     firstName.className = 'input invalid';
@@ -43,9 +43,9 @@ signupForm.addEventListener('click', function(event) {
    * people have hyphenated or apostrophe's in names.
    */
   if (!validator.isEmpty(lastName.value) && validator.isOfLength(lastName.value, 2)) {
-    lastName.className = 'input';
-    last.innerHTML = '';
-    last.removeAttribute('class', 'lastname-error');
+    lastName.className = 'input valid';
+    last.innerHTML = '&#10003';
+    last.className = 'valid';
   } else {
     lastName.className = 'input invalid';
     last.innerHTML = 'Please enter at least 2 characters';
@@ -57,9 +57,9 @@ signupForm.addEventListener('click', function(event) {
    * valid.
    */
   if (!validator.isEmpty(email.value) && validator.isEmailAddress(email.value)) {
-    email.className = 'input';
-    emailMsg.innerHTML = '';
-    emailMsg.removeAttribute('class', 'email-error');
+    email.className = 'input valid';
+    emailMsg.innerHTML = '&#10003';
+    emailMsg.className = 'valid';
   } else {
     email.className = 'input invalid';
     emailMsg.innerHTML = 'Please enter at valid email address';
@@ -71,9 +71,9 @@ signupForm.addEventListener('click', function(event) {
    * chararters long
    */
   if (!validator.isEmpty(password.value) && (validator.isOfLength(password.value, 6)) && (validator.isLength(password.value, 8))) {
-   password.className = 'input';
-   passwordMsg.innerHTML = '';
-   passwordMsg.removeAttribute('class', 'password-error');
+   password.className = 'input valid';
+   passwordMsg.innerHTML = '&#10003';
+   passwordMsg.className = 'valid';
   } else {
    password.className = 'input invalid';
    passwordMsg.innerHTML = 'Please enter at valid password';
@@ -86,9 +86,9 @@ signupForm.addEventListener('click', function(event) {
    * minimum age of 18.
    */
   if (!validator.isEmpty(birthdate.value) && (validator.isDate(birthdate.value) && validator.isBeforeToday(birthdate.value)) && age >= 18) {
-    birthdate.className = 'input';
-    birthdayMsg.innerHTML = '';
-    birthdayMsg.removeAttribute('class', 'birthdate-error');
+    birthdate.className = 'input valid';
+    birthdayMsg.innerHTML = '&#10003';
+    birthdayMsg.className = 'valid';
   } else {
     birthdate.className = 'input invalid';
     birthdayMsg.innerHTML = 'You must be older than 18';
@@ -96,7 +96,7 @@ signupForm.addEventListener('click', function(event) {
   }
 
   /**
-   * Checks that validation are valid.  If they are it sets the 
+   * Checks that validation are valid.  If they are it sets the
    * form.className to valid.
    */
   if (!validator.isEmpty(firstName) && validator.isOfLength(firstName, 2) && !validator.isEmpty(lastName) && validator.isOfLength(lastName, 2) && !validator.isEmpty(email) && validator.isEmailAddress(email) && !validator.isEmpty(password) && (validator.isOfLength(password, 6)) && (validator.isLength(password, 8)) && !validator.isEmpty(birthdate) &&
